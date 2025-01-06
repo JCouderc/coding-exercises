d1412cfad8affbcad65cfaf63361a34380f7d035
JCouderc <78983380+JCouderc@users.noreply.github.com>
2025-01-06 23:58:27 +0100

    feat: Add exercise 2 examples to main

--- src/main/java/dev/couderc/Main.java
+++ src/main/java/dev/couderc/Main.java
@@ -1,6 +1,11 @@
 package dev.couderc;
 
 import dev.couderc.exercise1.Exercise1;
+import dev.couderc.exercise2.Office;
+import dev.couderc.exercise2.exporter.DashSpreadsheetExporter;
+import dev.couderc.exercise2.exporter.StarSpreadsheetExporter;
+import dev.couderc.exercise2.spreadsheet.core.Spreadsheet;
+import dev.couderc.exercise2.spreadsheet.impl.SpreadsheetImpl;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,5 +18,14 @@ public class Main {
         System.out.println("Exercise 1");
         System.out.println(String.join(",", Exercise1.getDuplicates(List.of("b", "a", "c", "c", "e", "a", "c", "d", "c", "d"))));
 
+        System.out.println("\nExercise 2");
+        SpreadsheetImpl sheet = Office.newSpreadsheet(10, 5);
+        sheet.put(0, 0, "a");
+        sheet.put(1, 1, "b");
+        sheet.put(2, 2, "c");
+        sheet.put(3, 3, "d");
+        sheet.put(3, 4, "e");
+        System.out.println(new DashSpreadsheetExporter(sheet).export());
+        System.out.println(new StarSpreadsheetExporter(sheet).export());
     }
 }
